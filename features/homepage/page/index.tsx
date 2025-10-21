f192f3b3fd555bc813f63c58086835d7529e3aa8
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-10-21 21:32:31 +0700

    :lipstick: (ui) create cta button

--- features/homepage/page/index.tsx
+++ features/homepage/page/index.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import Jumbotron from "../ui/Jumbotron";
+import CtaCard from "../ui/CtaCard";
 
 const Homepage = () => {
   return (
     <div>
       <Jumbotron />
+      <CtaCard />
     </div>
   );
 };
---
+++ features/homepage/ui/CtaCard.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@heroui/react";
+import React from "react";
+
+const CtaCard = () => {
+  return (
+    <div className="mx-4 mt-8 flex flex-col gap-4">
+      <div className="w-full">
+        <Button className="w-full rounded-sm" radius="none" color="primary">
+          Cek Status Pembayaran
+        </Button>
+        <p className="text-xs text-center mt-1 text-neutral-600">
+          Lihat apakah iuran rumah Anda dan Tetangga Anda sudah tercatat minggu
+          ini.
+        </p>
+      </div>
+      <div>
+        <Button
+          className="w-full rounded-sm"
+          radius="none"
+          color="primary"
+          isDisabled
+        >
+          Laporan Keuangan
+        </Button>
+        <p className="text-xs text-center mt-1 text-neutral-600">
+          Segera hadir. pantau total iuran yang terkumpul dan penggunaannya
+          secara transparan.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CtaCard;
