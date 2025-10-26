0fd80bfd83a9b8a4574bdea3ea019576ca398cc1
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-10-26 23:49:31 +0700

    :construction: (wip) fetch household list before display the form

--- features/admin/page/CreateHousehold.tsx
+++ features/admin/page/CreateHousehold.tsx
@@ -1,13 +1,33 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TitleCreateHousehold from "../ui/CreateHousehold/Title";
 import FormCreateHousehold from "../ui/CreateHousehold/FormCard";
+import { HouseholdSimpleList } from "@/entities/household/types";
+import { getAllHousehold } from "@/entities/household/model/getAllHousehold";
+import LoadingScreen from "@/shared/ui/LoadingScreen";
 
 const CreateHousehold = () => {
+  const [householdList, setHouseholdList] = useState<
+    HouseholdSimpleList[] | null
+  >(null);
+
+  useEffect(() => {
+    (async () => {
+      const callback = await getAllHousehold();
+      setHouseholdList(callback);
+    })();
+  }, []);
+
   return (
     <div className="pt-8">
-      <TitleCreateHousehold />
-      <FormCreateHousehold />
+      {householdList ? (
+        <div>
+          <TitleCreateHousehold />
+          <FormCreateHousehold />
+        </div>
+      ) : (
+        <LoadingScreen />
+      )}
     </div>
   );
 };
