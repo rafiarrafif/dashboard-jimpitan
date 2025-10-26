f8c7c672a4cc5c60f2fc778e95ca2a117c1c2507
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-10-26 23:45:14 +0700

    :lipstick: (ui) add alert timeout
    Add an alert timeout if the loading time exceeds 8 seconds. This is useful for
    giving users the option to cancel a process that takes longer than expected.

--- shared/ui/LoadingScreen.tsx
+++ shared/ui/LoadingScreen.tsx
@@ -1,11 +1,29 @@
 "use client";
 import { Spinner } from "@heroui/react";
-import React from "react";
+import React, { useState } from "react";
 
 const LoadingScreen = () => {
+  const [loadingTimeout, setLoadingTimeout] = useState(false);
+
+  setTimeout(() => {
+    setLoadingTimeout(true);
+  }, 8000);
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-6">
       <Spinner className="-mt-42" />
+      {loadingTimeout ? (
+        <div className="text-sm text-center px-4 flex flex-col gap-2">
+          <p className="text-neutral-600">
+            Proses memuat data membutuhkan waktu lebih lama dari biasanya.
+          </p>
+          <a href="/" className="text-blue-600">
+            Kembali ke beranda
+          </a>
+        </div>
+      ) : (
+        <div />
+      )}
     </div>
   );
 };
