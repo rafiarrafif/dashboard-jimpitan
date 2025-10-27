30456c09061d0e297fdbd195c3274a567acf18a5
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-10-27 11:07:28 +0700

    :necktie: (necktie) add form register

--- entities/household/types.ts
+++ entities/household/types.ts
@@ -8,3 +8,9 @@ export type HouseholdSimpleList = {
   id: string;
   householdName: string;
 };
+export type CreateHouseholdFormData = {
+  headOfHousehold: string;
+  householdPronoun: string;
+  firstNeighborId?: string;
+  secondNeighborId?: string;
+};
--- features/admin/ui/CreateHousehold/FormCard.tsx
+++ features/admin/ui/CreateHousehold/FormCard.tsx
@@ -1,8 +1,11 @@
 "use client";
-import { getAllHousehold } from "@/entities/household/model/getAllHousehold";
-import { HouseholdSimpleList } from "@/entities/household/types";
+import React from "react";
+import {
+  CreateHouseholdFormData,
+  HouseholdSimpleList,
+} from "@/entities/household/types";
 import { Button, Form, Input, Select, SelectItem } from "@heroui/react";
-import React, { useEffect, useState } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 
 const pronounOptions = [
   { value: "pak", label: "Pak" },
@@ -17,91 +20,105 @@ const FormCreateHousehold = ({
 }: {
   householdList: HouseholdSimpleList[];
 }) => {
+  const formPayload = useForm<CreateHouseholdFormData>();
+  const { handleSubmit, register } = formPayload;
+
+  const sendFormData: SubmitHandler<CreateHouseholdFormData> = (data) =>
+    console.log(data);
+
   return (
-    <Form className="w-full px-4 mt-6">
-      <div className="w-full flex flex-col gap-2">
-        <Input
-          label="Nama Perwakilan Keluarga"
-          variant="bordered"
+    <FormProvider {...formPayload}>
+      <Form className="w-full px-4 mt-6" onSubmit={handleSubmit(sendFormData)}>
+        <div className="w-full flex flex-col gap-2">
+          <Input
+            {...register("headOfHousehold")}
+            label="Nama Perwakilan Keluarga"
+            variant="bordered"
+            radius="none"
+            classNames={{
+              inputWrapper: "border-neutral-400 border-1 rounded-sm",
+            }}
+            required
+          />
+          <Select
+            {...register("householdPronoun")}
+            items={pronounOptions}
+            label="Sapaan"
+            variant="bordered"
+            radius="none"
+            classNames={{
+              trigger: "border-neutral-400 border-1 rounded-sm",
+              popoverContent: "rounded-sm",
+            }}
+            required
+          >
+            {(pronounOptions) => (
+              <SelectItem
+                key={pronounOptions.value}
+                classNames={{
+                  base: "rounded-sm",
+                }}
+              >
+                {pronounOptions.label}
+              </SelectItem>
+            )}
+          </Select>
+          <Select
+            {...register("firstNeighborId")}
+            items={householdList}
+            label="Nama Tetangga 1 (Opsional)"
+            variant="bordered"
+            radius="none"
+            classNames={{
+              trigger: "border-neutral-400 border-1 rounded-sm",
+              popoverContent: "rounded-sm",
+            }}
+          >
+            {(householdList) => (
+              <SelectItem
+                key={householdList.id}
+                classNames={{
+                  base: "rounded-sm",
+                }}
+              >
+                {householdList.householdName}
+              </SelectItem>
+            )}
+          </Select>
+          <Select
+            {...register("secondNeighborId")}
+            items={householdList}
+            label="Nama Tetangga 2 (Opsional)"
+            variant="bordered"
+            radius="none"
+            classNames={{
+              trigger: "border-neutral-400 border-1 rounded-sm",
+              popoverContent: "rounded-sm",
+            }}
+          >
+            {(householdList) => (
+              <SelectItem
+                key={householdList.id}
+                classNames={{
+                  base: "rounded-sm",
+                }}
+              >
+                {householdList.householdName}
+              </SelectItem>
+            )}
+          </Select>
+        </div>
+        <Button
+          type="submit"
+          variant="shadow"
+          color="primary"
+          className="w-full rounded-sm text-white h-12 mt-2"
           radius="none"
-          classNames={{
-            inputWrapper: "border-neutral-400 border-1 rounded-sm",
-          }}
-        />
-        <Select
-          items={pronounOptions}
-          label="Sapaan"
-          variant="bordered"
-          radius="none"
-          classNames={{
-            trigger: "border-neutral-400 border-1 rounded-sm",
-            popoverContent: "rounded-sm",
-          }}
         >
-          {(pronounOptions) => (
-            <SelectItem
-              key={pronounOptions.value}
-              classNames={{
-                base: "rounded-sm",
-              }}
-            >
-              {pronounOptions.label}
-            </SelectItem>
-          )}
-        </Select>
-        <Select
-          items={householdList}
-          label="Nama Tetangga 1 (Opsional)"
-          variant="bordered"
-          radius="none"
-          classNames={{
-            trigger: "border-neutral-400 border-1 rounded-sm",
-            popoverContent: "rounded-sm",
-          }}
-        >
-          {(householdList) => (
-            <SelectItem
-              key={householdList.id}
-              classNames={{
-                base: "rounded-sm",
-              }}
-            >
-              {householdList.householdName}
-            </SelectItem>
-          )}
-        </Select>
-        <Select
-          items={householdList}
-          label="Nama Tetangga 2 (Opsional)"
-          variant="bordered"
-          radius="none"
-          classNames={{
-            trigger: "border-neutral-400 border-1 rounded-sm",
-            popoverContent: "rounded-sm",
-          }}
-        >
-          {(householdList) => (
-            <SelectItem
-              key={householdList.id}
-              classNames={{
-                base: "rounded-sm",
-              }}
-            >
-              {householdList.householdName}
-            </SelectItem>
-          )}
-        </Select>
-      </div>
-      <Button
-        type="button"
-        variant="shadow"
-        color="primary"
-        className="w-full rounded-sm text-white h-12 mt-2"
-        radius="none"
-      >
-        Tambah
-      </Button>
-    </Form>
+          Tambah
+        </Button>
+      </Form>
+    </FormProvider>
   );
 };
 
