b2316781e3899d874b91a57fefbbc1e53edb6463
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-10-30 23:39:24 +0700

    :triangular_flag_on_post: (flags) add admin page

--- app/auth/login/google/page.tsx
+++
@@ -1,9 +0,0 @@
-import { signIn } from "next-auth/react";
-import React from "react";
-
-const page = async () => {
-  await signIn("google", { redirectTo: "/" });
-  return <div>page</div>;
-};
-
-export default page;
