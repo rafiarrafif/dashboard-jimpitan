d8320181fce1f907899beb6cbbf7390050064692
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-10-30 23:42:48 +0700

    :necktie: (necktie) add middleware for auth page
    prevent authenticated user for accessing auth page with middleware.

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,18 @@ import { auth } from "./auth";
 
 export const middleware = async (request: NextRequest) => {
   const rootUrl = new URL("/auth/login", request.url);
+  const adminUrl = new URL("/admin", request.url);
   const { pathname } = request.nextUrl;
 
   if (pathname.startsWith("/admin")) {
     const session = await auth();
     if (!session?.user) return NextResponse.redirect(rootUrl);
+    NextResponse.next();
+  }
+
+  if (pathname.startsWith("/auth")) {
+    const session = await auth();
+    if (session?.user) return NextResponse.redirect(adminUrl);
     NextResponse.next();
   }
 };
