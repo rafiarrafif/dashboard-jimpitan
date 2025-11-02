52f5db1e3ad5f31cb4d5c465ba5d1568284c8b52
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-02 21:49:27 +0700

    :lipstick: (ui) updating scan button in bottom navbar

---
+++ app/admin/scan/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Example page scan</div>;
+};
+
+export default page;
--- features/admin/ui/shared/BottomNavbar.tsx
+++ features/admin/ui/shared/BottomNavbar.tsx
@@ -24,7 +24,16 @@ const BottomNavbar = () => {
           <span className="text-sm">Dashboard</span>
         </Button>
         <div className="w-[30vw] h-full py-2 ">
-          <Button className="h-full w-full flex flex-col items-center justify-center gap-1 bg-primary text-neutral-100 rounded-sm">
+          <Button
+            variant="bordered"
+            className={`h-full w-full flex flex-col items-center justify-center gap-1 rounded-sm ${
+              route === "/admin/scan"
+                ? "bg-transparent !border-3 border-neutral-700 text-primary"
+                : "bg-primary text-neutral-100"
+            }`}
+            as={Link}
+            href="/admin/scan"
+          >
             <Icon icon="bx:qr-scan" className="text-2xl" />
             <span className="text-sm">Scan</span>
           </Button>
