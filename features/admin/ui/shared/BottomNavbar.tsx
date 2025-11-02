a1a8b77a79d3820857144ea8c429320aacb54f4d
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-02 17:39:58 +0700

    :bug: (fix) fix link navigation in bottom navbar
    change link element to button for better user experience.

--- features/admin/ui/shared/BottomNavbar.tsx
+++ features/admin/ui/shared/BottomNavbar.tsx
@@ -13,30 +13,34 @@ const BottomNavbar = () => {
         className="w-full h-full flex flex-row px-4gap-2"
         classNames={{ base: "rounded-sm" }}
       >
-        <Link
-          className={`flex-1 flex flex-col items-center justify-center gap-1 ${
+        <Button
+          className={`h-full flex-1 flex flex-col items-center justify-center gap-1 bg-transparent rounded-none ${
             route === "/admin" ? "text-primary" : "text-neutral-400"
           }`}
+          as={Link}
+          href="/admin"
         >
           <Icon icon="ph:squares-four-fill" className="text-2xl" />
           <span className="text-sm">Dashboard</span>
-        </Link>
+        </Button>
         <div className="w-[30vw] h-full py-2 ">
           <Button className="h-full w-full flex flex-col items-center justify-center gap-1 bg-primary text-neutral-100 rounded-sm">
             <Icon icon="bx:qr-scan" className="text-2xl" />
             <span className="text-sm">Scan</span>
           </Button>
         </div>
-        <Link
-          className={`flex-1 flex flex-col items-center justify-center gap-1 ${
+        <Button
+          className={`h-full flex-1 flex flex-col items-center justify-center gap-1 bg-transparent rounded-none ${
             route.startsWith("/admin/household")
               ? "text-primary"
               : "text-neutral-400"
           }`}
+          as={Link}
+          href="/admin/household"
         >
           <Icon icon="ri:home-5-fill" className="text-2xl" />
           <span className="text-sm">Iuran Rumah</span>
-        </Link>
+        </Button>
       </Card>
     </div>
   );
