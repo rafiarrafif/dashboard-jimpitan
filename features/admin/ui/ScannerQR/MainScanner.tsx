2f6cc67d28aaf4b599db5fdbaa1919192a14827e
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-07 22:10:21 +0700

    :triangular_flag_on_post: (flags) add pause time in scanner

--- features/admin/ui/ScannerQR/MainScanner.tsx
+++ features/admin/ui/ScannerQR/MainScanner.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import { Scanner } from "@yudiel/react-qr-scanner";
-import React from "react";
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+import React, { useState } from "react";
 
 const MainScanner = () => {
+  const [cameraPaused, setCameraPaused] = useState(false);
+
+  const onScanHandler = (result: IDetectedBarcode[]) => {
+    console.log(`berhasil! ${JSON.stringify(result)}`);
+    setCameraPaused(true);
+    setTimeout(() => {
+      setCameraPaused(false);
+    }, 3000);
+  };
+
   return (
     <div>
       <Scanner
         constraints={{ facingMode: "environment" }}
-        onScan={(result) => console.log(result)}
+        onScan={onScanHandler}
         onError={(error) => console.log(error)}
+        paused={cameraPaused}
       />
     </div>
   );
