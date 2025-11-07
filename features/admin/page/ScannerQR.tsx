69eadf208950388283ed02027a5dacb74b472dd2
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-07 17:14:43 +0700

    :triangular_flag_on_post: (flags) create loading screen and request validation
    Add a loading screen and camera access validation. If these requirements are
    met, the QR scan component will be rendered. If not, an error message will be
    displayed.

--- features/admin/page/ScannerQR.tsx
+++ features/admin/page/ScannerQR.tsx
@@ -1,17 +1,37 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { Spinner } from "@heroui/react";
 import MainScanner from "../ui/ScannerQR/MainScanner";
 
 const ScannerQR = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const videoInputs = devices.filter((d) => d.kind === "videoinput");
+      console.log(`data dari input vide: ${JSON.stringify(videoInputs)}`);
+      videoInputs[0].deviceId
+        ? setHasCameraPermission(true)
+        : setHasCameraPermission(false);
+    });
+  }, []);
+
   return (
     <div>
       <div className="text-center my-6">
         <h1 className="text-2xl font-semibold">Scan Jimpitan</h1>
         <p>Arahkan kamera ke QR Code</p>
       </div>
-      <div className="mx-8 rounded-sm overflow-hidden shadow-lg border border-neutral-400">
-        <MainScanner />
+      <div className="mx-8 rounded-sm overflow-hidden shadow-lg border border-neutral-300">
+        {hasCameraPermission ? (
+          <MainScanner />
+        ) : (
+          <div className="h-48 bg-neutral-50 px-2 flex flex-col items-center justify-center gap-4 ">
+            <Spinner />
+            <span>Menghubungkan ke kamera</span>
+          </div>
+        )}
       </div>
     </div>
   );
