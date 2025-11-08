f6eab75ec5067542397f16c19e038953dbf47633
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-08 11:12:11 +0700

    :sparkles: (feat) add code input
    add input code just in case the camera cannot be used for scanning.

--- features/admin/page/ScannerQR.tsx
+++ features/admin/page/ScannerQR.tsx
@@ -1,49 +1,16 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { Spinner } from "@heroui/react";
-import MainScanner from "../ui/ScannerQR/MainScanner";
+import React from "react";
+import ScannerOrCode from "../ui/ScannerQR/ScannerOrCode";
 
 const ScannerQR = () => {
-  const [hasCameraPermission, setHasCameraPermission] = useState(false);
-  const [loadingText, setLoadingText] = useState("Menghubungkan ke kamera");
-
-  const getPermission = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(() => {
-        setHasCameraPermission(true);
-        window.location.reload();
-      })
-      .catch(() => {
-        setHasCameraPermission(false);
-        setLoadingText("Kamera tidak dapat terhubung");
-      });
-  };
-
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then((devices) => {
-      const videoInputs = devices.filter((d) => d.kind === "videoinput");
-      videoInputs[0].deviceId ? setHasCameraPermission(true) : getPermission();
-    });
-  }, []);
-
   return (
     <div>
       <div className="text-center my-6">
         <h1 className="text-2xl font-semibold">Scan Jimpitan</h1>
         <p>Arahkan kamera ke QR Code</p>
       </div>
-      <div className="mx-8 rounded-sm overflow-hidden shadow-lg border border-neutral-300">
-        {hasCameraPermission ? (
-          <MainScanner />
-        ) : (
-          <div className="h-48 bg-neutral-50 px-2 flex flex-col items-center justify-center gap-4 ">
-            <Spinner />
-            <span>{loadingText}</span>
-          </div>
-        )}
-      </div>
+      <ScannerOrCode />
     </div>
   );
 };
---
+++ features/admin/ui/ScannerQR/ScannerOrCode.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Button, InputOtp, Link, Spinner } from "@heroui/react";
+import MainScanner from "./MainScanner";
+
+const ScannerOrCode = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [loadingText, setLoadingText] = useState("Menghubungkan ke kamera");
+
+  const getPermission = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(() => {
+        setHasCameraPermission(true);
+        window.location.reload();
+      })
+      .catch(() => {
+        setHasCameraPermission(false);
+        setLoadingText("Kamera tidak dapat terhubung");
+      });
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const videoInputs = devices.filter((d) => d.kind === "videoinput");
+      videoInputs[0].deviceId ? setHasCameraPermission(true) : getPermission();
+    });
+  }, []);
+
+  const [useCameraMethod, setUseCameraMethod] = useState(true);
+  return (
+    <div>
+      {useCameraMethod ? (
+        <div>
+          <div className="mx-8 rounded-sm overflow-hidden shadow-lg border border-neutral-300">
+            {hasCameraPermission ? (
+              <MainScanner />
+            ) : (
+              <div className="h-48 bg-neutral-50 px-2 flex flex-col items-center justify-center gap-4 ">
+                <Spinner />
+                <span>{loadingText}</span>
+              </div>
+            )}
+          </div>
+          <div className="w-full flex justify-center mt-3">
+            <span className="text-sm text-neutral-600">
+              Kamera bermasalah?{" "}
+              <Link
+                onPress={() => setUseCameraMethod(false)}
+                className="text-blue-400 text-sm"
+              >
+                masukan code
+              </Link>
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center w-full pt-4">
+          <InputOtp
+            classNames={{
+              segmentWrapper: "gap-x-0",
+              segment: [
+                "relative",
+                "h-10",
+                "w-10",
+                "border-y",
+                "border-r",
+                "first:rounded-l-md",
+                "first:border-l",
+                "last:rounded-r-md",
+                "border-default-400",
+                "data-[active=true]:border",
+                "data-[active=true]:z-20",
+                "data-[active=true]:ring-2",
+                "data-[active=true]:ring-offset-2",
+                "data-[active=true]:ring-offset-background",
+                "data-[active=true]:ring-foreground",
+              ],
+            }}
+            length={8}
+            radius="none"
+          />
+          <div className="w-full flex justify-center mt-2">
+            <span className="text-sm text-neutral-600">
+              Pakai kamera?{" "}
+              <Link
+                onPress={() => setUseCameraMethod(true)}
+                className="text-blue-400 text-sm"
+              >
+                gunakan scanner
+              </Link>
+            </span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ScannerOrCode;
