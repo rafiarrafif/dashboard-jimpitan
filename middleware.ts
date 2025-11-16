08bd62ba7abf5463aa4505678b17347283873359
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-16 15:18:14 +0700

    :heavy_minus_sign: (dep-rm) Remove authjs dependency
    Removing the authjs dependency because it will be replaced with better-auth for
    better Prisma integration.

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./auth";
 
 export const middleware = async (request: NextRequest) => {
-  const loginUrl = new URL("/auth/login", request.url); // redirect to login page
-  const adminUrl = new URL("/admin", request.url); // redirect to admin page
-  const { pathname } = request.nextUrl;
+  // const loginUrl = new URL("/auth/login", request.url); // redirect to login page
+  // const adminUrl = new URL("/admin", request.url); // redirect to admin page
+  // const { pathname } = request.nextUrl;
 
-  // middleware for admin routes (/admin/*)
-  if (pathname.startsWith("/admin")) {
-    const session = await auth();
-    if (!session?.user) return NextResponse.redirect(loginUrl);
-    NextResponse.next();
-  }
+  // // middleware for admin routes (/admin/*)
+  // if (pathname.startsWith("/admin")) {
+  //   const session = await auth();
+  //   if (!session?.user) return NextResponse.redirect(loginUrl);
+  //   NextResponse.next();
+  // }
 
-  // middleware for auth routes (/auth/*)
-  if (pathname.startsWith("/auth")) {
-    const session = await auth();
-    if (session?.user) return NextResponse.redirect(adminUrl);
-    NextResponse.next();
-  }
+  // // middleware for auth routes (/auth/*)
+  // if (pathname.startsWith("/auth")) {
+  //   const session = await auth();
+  //   if (session?.user) return NextResponse.redirect(adminUrl);
+  //   NextResponse.next();
+  // }
+
+  NextResponse.next();
 };
