a00ebbddb040cdcd94c4b09bbe2e65a709ceee35
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-11-19 15:42:55 +0700

    :lock: (sec) enable middleware service
    Reactivating the middleware with a new configuration that supports better-auth.

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
 export const middleware = async (request: NextRequest) => {
-  // const loginUrl = new URL("/auth/login", request.url); // redirect to login page
-  // const adminUrl = new URL("/admin", request.url); // redirect to admin page
-  // const { pathname } = request.nextUrl;
+  const loginUrl = new URL("/auth/login", request.url); // redirect to login page
+  const adminUrl = new URL("/admin", request.url); // redirect to admin page
+  const { pathname } = request.nextUrl;
+  const session = getSessionCookie(request);
 
-  // // middleware for admin routes (/admin/*)
-  // if (pathname.startsWith("/admin")) {
-  //   const session = await auth();
-  //   if (!session?.user) return NextResponse.redirect(loginUrl);
-  //   NextResponse.next();
-  // }
+  // middleware for admin routes (/admin/*)
+  if (pathname.startsWith("/admin")) {
+    if (!session) return NextResponse.redirect(loginUrl);
+    NextResponse.next();
+  }
 
-  // // middleware for auth routes (/auth/*)
-  // if (pathname.startsWith("/auth")) {
-  //   const session = await auth();
-  //   if (session?.user) return NextResponse.redirect(adminUrl);
-  //   NextResponse.next();
-  // }
+  // middleware for auth routes (/auth/*)
+  if (pathname.startsWith("/auth")) {
+    if (session) return NextResponse.redirect(adminUrl);
+    NextResponse.next();
+  }
 
   NextResponse.next();
 };
