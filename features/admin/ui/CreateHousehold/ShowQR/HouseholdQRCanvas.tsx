12d882b521d8eb7ed97b977b06ca94493c28582d
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-12-07 15:22:51 +0700

    :heavy_plus_sign: (dep-add) tsx2pdf package to save household qr

--- features/admin/ui/CreateHousehold/ShowQR/HouseholdQRCanvas.tsx
+++ features/admin/ui/CreateHousehold/ShowQR/HouseholdQRCanvas.tsx
@@ -1,17 +1,27 @@
 "use client";
 
 import { HouseholdMetaQR } from "@/entities/household/model/getHouseholdMetaQR";
+import { usePDF } from "react-to-pdf";
 import QRCode from "react-qr-code";
 import React from "react";
+import { Button } from "@heroui/react";
 
 const HouseholdQRCanvas = ({
   householdData,
 }: {
   householdData: HouseholdMetaQR;
 }) => {
+  const { toPDF, targetRef } = usePDF({
+    filename: `qr-${householdData.householdName
+      .trim()
+      .toLowerCase()
+      .split(" ")
+      .join("-")}.pdf`,
+  });
+
   return (
     <div>
-      <main className="mx-4 pt-6 flex justify-center">
+      <main ref={targetRef} className="mx-4 pt-6 flex justify-center">
         <div className="bg-primary w-fit rounded-md flex flex-col items-center p-2">
           <div className="py-2">
             <span className="font-medium text-neutral-100 text-2xl">
@@ -35,6 +45,18 @@ const HouseholdQRCanvas = ({
           </div>
         </div>
       </main>
+      <div className="mt-6 mx-8">
+        <Button
+          as="button"
+          className="w-full rounded-sm text-white h-14"
+          radius="none"
+          color="primary"
+          href="cek-iuran"
+          onPress={() => toPDF()}
+        >
+          Download as PDF
+        </Button>
+      </div>
     </div>
   );
 };
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname, // enables Turbopack for the `/src` directory
+    resolveAlias: {
+      html2canvas: "html2canvas-pro",
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      html2canvas: "html2canvas-pro",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
