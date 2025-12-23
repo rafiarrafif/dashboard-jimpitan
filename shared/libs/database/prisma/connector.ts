f67bdca11833203b8778b4904f23901c7bcd904f
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-12-23 23:05:53 +0700

    :heavy_plus_sign: (dep-add) prisma accelerate

--- shared/libs/database/prisma/connector.ts
+++ shared/libs/database/prisma/connector.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { adapter } from "./adapter";
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
