beadb9265c8235340bf29a357b7316acbe8066ad
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-12-24 23:56:36 +0700

    :rotating_light: (lint) fixing linting warning

--- auth.ts
+++ auth.ts
@@ -1,13 +1,22 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./shared/libs/database/prisma/connector";
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import Google from "next-auth/providers/google";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      realName: string | null;
+      realId: string | null;
+    } & DefaultSession["user"];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
   callbacks: {
-    async session({ session, token }) {
+    async session({ session }) {
       const collector = await prisma.collector.findUnique({
         where: { email: session.user.email ?? "" },
         select: {
