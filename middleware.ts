967f83563371342fcdc8d6959145698b8540143d
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-12-27 20:23:53 +0700

    :bulb: (docs-code) create middleware route documentation

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ export async function middleware(req: NextRequest) {
   const adminUrl = new URL("/admin", req.url);
   const blockedUrl = new URL("/auth/blocked", req.url);
 
+  // Route to admin page (only authorized account)
   if (pathname.startsWith("/admin")) {
     const token = await getToken({ req, secret: process.env.AUTH_SECRET });
     if (!token) return NextResponse.redirect(loginUrl);
@@ -16,12 +17,14 @@ export async function middleware(req: NextRequest) {
     return NextResponse.next();
   }
 
+  // Route to account blocked page (only login user without realId)
   if (pathname.startsWith("/auth/blocked")) {
     if (!token) return NextResponse.redirect(loginUrl);
     if (token?.realId) return NextResponse.redirect(adminUrl);
     return NextResponse.next();
   }
 
+  // Route to auth login page (only unauthenticated user)
   if (pathname.startsWith("/auth/login")) {
     if (token) return NextResponse.redirect(adminUrl);
     NextResponse.next();
